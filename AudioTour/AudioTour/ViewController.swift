238460ae20f4d21944f3d2b1e0e4940089b0b549
NickPuchko <47064996+NickPuchko@users.noreply.github.com>
2021-03-26 22:06:54 +0300

    Pods, SwiftLint

---
+++ AudioTour/AudioTour/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AudioTour
+//
+//  Created by Administrator on 26.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
