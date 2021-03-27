f2eccf6993612e5b92309160509934091fef37bc
NickPuchko <puchk2000n@gmail.com>
2021-03-27 15:37:41 +0300

    Feed cells

--- AudioTour/AudioTour/Feed/FeedView.swift
+++ AudioTour/AudioTour/Feed/FeedView.swift
@@ -10,18 +10,29 @@ import MapKit
 
 class FeedView: UIView {
     
-    init() {
+//    let searchVC = UISearchController()
+    
+    let toursTableView = UITableView(frame: .zero, style: .plain)
+    
+    init(delegate: FeedViewController) {
         super.init(frame: .zero)
+        
+        toursTableView.delegate = delegate
+        toursTableView.dataSource = delegate
         
         backgroundColor = .white
     }
     
-    private func setupMapView() {
+    private func setupTableView() {
         
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
+            toursTableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            toursTableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            toursTableView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            toursTableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
         ])
     }
     
