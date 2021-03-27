0d8116957797e775d83aad1c53c9de40ff6583d2
maksim <staliniv2001@gmail.com>
2021-03-27 16:21:24 +0300

    add all requests

--- AudioTour/AudioTour/Feed/FeedViewController.swift
+++ AudioTour/AudioTour/Feed/FeedViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class FeedViewController: UIViewController {
-
+    var a = NetworkManager()
+    var player: AVAudioPlayer?
     private var feedView: FeedView {
         view as! FeedView
     }
@@ -19,6 +21,49 @@ class FeedViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        a.fetchImage(uuid: "d319471a-1108-4d97-a938-b82ed956f138", image_uuid: "ab9ac94d-61ca-4167-83f3-cf31804c92b6_800x600") { (result) in
+//            switch result {
+//            case .success(let imageData):
+//                self.view.backgroundColor = UIColor(patternImage: UIImage(data: imageData)!)
+//            case .failure(let error):
+//                print("Errror \(error.localizedDescription)")
+//            }
+//        }
+        a.fetchAudio(uuid: "d319471a-1108-4d97-a938-b82ed956f138", audio_uuid: "357cc390-81f2-46ea-b3b7-5a0800c1e69e") { (result) in
+            switch result {
+            case .success(let dataAudio):
+                
+                if let player = self.player, player.isPlaying {
+                    
+                }
+                else {
+                    do {
+                        
+                        try AVAudioSession.sharedInstance().setMode(.default)
+                        try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                        self.player = try AVAudioPlayer(data: dataAudio)
+                        guard let player = self.player else {
+                            print("Player")
+                            return
+                        }
+                        player.play()
+                    }
+                    catch {
+                        print("something wrong")
+                    }
+                }
+            case .failure(let error):
+                print("Errror \(error.localizedDescription)")
+            }
+        }
+//        a.fetchDataTour(symbol: "Мурманск") { (result) in
+//            switch result {
+//            case .success(let tours):
+//                print(tours)
+//            case .failure(let error):
+//                print("Errror \(error.localizedDescription)")
+//            }
+//        }
         title = "City"
         // Do any additional setup after loading the view.
     }
--- AudioTour/AudioTour/NetworkManager.swift
+++ AudioTour/AudioTour/NetworkManager.swift
@@ -6,9 +6,11 @@
 //
 
 import Foundation
+
 protocol NetworkManagerProtocol {
-    func fetchDataTour(symbol: String, completion: @escaping (Result<[WelcomeElement], Error>) -> Void)
+    func fetchDataTour(symbol: String, completion: @escaping (Result<[SearchObjectsRoutesElement], Error>) -> Void)
 }
+
 class NetworkManager {
     
     private var dataTask: URLSessionDataTask?
@@ -21,14 +23,59 @@ class NetworkManager {
         result.path = "/mtg/objects/search/"
         result.query = "languages=ru,en&includes=all&api_key=\(token)&query=\(symbol)"
         return result.url
-//        let url = "https://api.izi.travel/mtg/objects/search?languages=ru,en&includes=all&api_key=7c6c2db9-d237-4411-aa0e-f89125312494&query="
-//            + symbol
     }
+    private func makeUrlForAudio(uuid: String, audio_uuid: String) -> URL? {
+        let token = "7c6c2db9-d237-4411-aa0e-f89125312494"
+        var result = URLComponents()
+        result.scheme = "https"
+        result.host = "media.izi.travel"
+        result.path = "/\(uuid)/\(audio_uuid).m4a"
+        result.query = "api_key=\(token)"
+        return result.url
+    }
+    private func makeUrlForImage(uuid: String, image_uuid: String) -> URL? {
+        let token = "7c6c2db9-d237-4411-aa0e-f89125312494"
+        var result = URLComponents()
+        result.scheme = "https"
+        result.host = "media.izi.travel"
+        result.path = "/\(uuid)/\(image_uuid).jpg"
+        result.query = "api_key=\(token)"
+        return result.url
+    }
+    
 }
 extension NetworkManager {
-    func fetchDataTour(symbol: String, completion: @escaping (Result<[WelcomeElement], Error>) -> Void) {
+    func fetchDataTour(symbol: String, completion: @escaping (Result<[SearchObjectsRoutesElement], Error>) -> Void) {
         let tourURL = makeUrl(for: symbol)
         dataTask = URLSession.shared.dataTask(with: tourURL!) { (data, response, error) in
+            
+            if let error = error {
+               print("DataTask error: \(error.localizedDescription)")
+               return
+            }
+            guard let response = response as? HTTPURLResponse, let data = data else {
+               print("Empty Response")
+               return
+            }
+            print("Response status code: \(response.statusCode)")
+            do {
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(SearchObjectsRoutesList.self, from: data)
+                print(jsonData)
+                DispatchQueue.main.async {
+                    completion(.success(jsonData))
+                }
+            }
+            catch let error {
+                completion(.failure(error))
+            }
+        }
+        dataTask?.resume()
+    }
+    
+    func fetchAudio(uuid: String, audio_uuid: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        let audioURL = makeUrlForAudio(uuid: uuid, audio_uuid: audio_uuid)
+        dataTask = URLSession.shared.dataTask(with: audioURL!) { (data, response, error) in
             if let error = error {
                print("DataTask error: \(error.localizedDescription)")
                return
@@ -38,18 +85,27 @@ extension NetworkManager {
                return
            }
            print("Response status code: \(response.statusCode)")
-           do {
-            //print(String(decoding: data, as: UTF8.self))
-            let decoder = JSONDecoder()
-            let jsonData = try decoder.decode(Welcome.self, from: data)
-            print(jsonData)
             DispatchQueue.main.async {
-                completion(.success(jsonData))
+                completion(.success(data))
             }
+        }
+        dataTask?.resume()
+    }
+    func fetchImage(uuid: String, image_uuid: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        let audioURL = makeUrlForImage(uuid: uuid, image_uuid: image_uuid)
+        dataTask = URLSession.shared.dataTask(with: audioURL!) { (data, response, error) in
+            if let error = error {
+               print("DataTask error: \(error.localizedDescription)")
+               return
+            }
+           guard let response = response as? HTTPURLResponse, let data = data else {
+               print("Empty Response")
+               return
            }
-           catch let error {
-                completion(.failure(error))
-           }
+           print("Response status code: \(response.statusCode)")
+            DispatchQueue.main.async {
+                completion(.success(data))
+            }
         }
         dataTask?.resume()
     }
