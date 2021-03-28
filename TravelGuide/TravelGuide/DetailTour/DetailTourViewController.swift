4970f201e8a9182367ed67f90be1d137337922d3
maksim <staliniv2001@gmail.com>
2021-03-28 05:43:55 +0300

    add avfoundation

--- TravelGuide/TravelGuide/DetailTour/DetailTourViewController.swift
+++ TravelGuide/TravelGuide/DetailTour/DetailTourViewController.swift
@@ -7,8 +7,11 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class DetailTourViewController: UIViewController{
+    
+    var player: AVAudioPlayer?
     
     var detailTourModel: DetailTourModel!
     private let detailTourView = DetailTourView()
@@ -20,5 +23,37 @@ class DetailTourViewController: UIViewController{
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        sleep(3)
+        showAudioAlert()
+    }
+    
+    private func showAudioAlert(){
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Продолжить с аудиогидом", style: .default, handler: { _ in
+            print("push")
+        }))
+        alert.addAction(UIAlertAction(title: "Оставьте меня в тишине!", style: .default, handler: { _ in
+            print("push")
+        }))
+        alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+//        if let player = player, player.isPlaying {
+//            player.stop()
+//        }
+//        else {
+//            do{
+//                try AVAudioSession.sharedInstance().setMode(.default)
+//                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+//                player = try AVAudioPlayer(data: <#T##Data#>)
+//
+//                guard let player  = player else {
+//                    return
+//                }
+//                player.play()
+//            }
+//            catch (let error){
+//                print(error.localizedDescription)
+//            }
+//        }
     }
 }
