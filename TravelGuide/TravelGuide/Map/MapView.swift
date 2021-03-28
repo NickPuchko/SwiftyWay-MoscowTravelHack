f1cbbb8d134010aabcf4df4cbeabba3a5ccbcd7b
NickPuchko <puchk2000n@gmail.com>
2021-03-28 06:21:57 +0300

    containerVIew

--- TravelGuide/TravelGuide/Map/MapView.swift
+++ TravelGuide/TravelGuide/Map/MapView.swift
@@ -11,11 +11,15 @@ import MapKit
 class MapView: UIView {
 
     let mapView: MKMapView
+    let containerView: UIView
     
     init(delegate: MapViewController) {
+        containerView = UIView()
+        containerView.backgroundColor = .systemGray6
         mapView = MKMapView()
         mapView.delegate = delegate
         super.init(frame: .zero)
+        addSubview(containerView)
         addSubview(mapView)
         setupConstraints()
     }
@@ -25,12 +29,18 @@ class MapView: UIView {
     }
     
     private func setupConstraints() {
+        containerView.translatesAutoresizingMaskIntoConstraints = false
         mapView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             mapView.leadingAnchor.constraint(equalTo: leadingAnchor),
             mapView.trailingAnchor.constraint(equalTo: trailingAnchor),
             mapView.topAnchor.constraint(equalTo: topAnchor),
-            mapView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            mapView.bottomAnchor.constraint(equalTo: containerView.topAnchor),
+            
+            containerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            containerView.heightAnchor.constraint(equalToConstant: 100)
         ])
     }
     
